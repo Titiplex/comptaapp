e171c53d8b99be69557db9f7326e1876d70445d4
Titiplex <107863779+Titiplex@users.noreply.github.com>
2025-07-12 18:46:33 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.titiplex.comptaapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.titiplex.comptaapp to javafx.fxml;
+    exports com.titiplex.comptaapp;
+}
